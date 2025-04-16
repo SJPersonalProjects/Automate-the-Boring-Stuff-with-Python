3c6bc7a6601a9f55c24d93bfa5b349da5b5d5ac8
Sarim Jokhio <sarimjokhioprivate@gmail.com>
2025-04-16 12:10:39 +0500

    Name checker

--- HelloWorld.py
+++ HelloWorld.py
@@ -7,11 +7,16 @@ print("What's your name?")
 myName = input()
 
 print("It's good to meet ya, " + myName)
-print("The length of your name is ")
-print(len(myName))
+print("The length of your name is ", len(myName))
+print("The length of your name is " + str(len(myName)))
 
 # Asks for age.
 print("What's your age?")
 myAge = input()
 
 print("You'll be " + str(int(myAge) + 1) + " in a year.")
+
+# Converting types.
+
+print() # Just for a gap.
+print(str(0))
---
+++ vampire.py
@@ -0,0 +1,12 @@
+# Vampire identity information.
+name = 'Carol'
+age = 3000
+
+if name == 'Alice':
+    print('Hi, Alice')
+elif age < 12:
+    print("You're not Alice, Kiddo")
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire')
+elif age > 100:
+    print("You're not Alice, grannie.")
