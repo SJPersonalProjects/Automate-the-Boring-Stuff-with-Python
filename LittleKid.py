5f69599e80054fb9891f32b15e48be16a6993cc3
Sarim Jokhio <sarimjokhioprivate@gmail.com>
2025-04-17 04:22:18 +0500

    Program to checks for the name

---
+++ LittleKid.py
@@ -0,0 +1,11 @@
+# Vampire identity information.
+name = 'Carol'
+age = 3000
+
+if name == 'Alice':
+    print("Hi, Alice")
+elif age < 12:
+    print("You're not Alice, Kiddo!")
+else :
+    print("You're neither Alice nor a little kid.")
+    
