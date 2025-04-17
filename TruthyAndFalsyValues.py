33a5f1579bb1fd13050980dd3b3130c184a5b8d9
Sarim Jokhio <sarimjokhioprivate@gmail.com>
2025-04-17 05:19:23 +0500

    Shows the importance of truthy and falsy values

---
+++ TruthyAndFalsyValues.py
@@ -0,0 +1,12 @@
+# Demonstrates the truthy and fasly values.
+
+name = ''
+while not name:
+    print("Enter your name")
+    name = input()
+
+print("How many guests will you have?")
+numberOfGuests = int(input())
+if numberOfGuests:
+    print("Be sure to have enough room for all your guests.")
+print("Done")
