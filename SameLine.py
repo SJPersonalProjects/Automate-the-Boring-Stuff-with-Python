95a82dd63a225adde6627d077f77b7a1d4a04837
Sarim Jokhio <sarimjokhioprivate@gmail.com>
2025-04-18 14:20:41 +0500

    end='' and sep='' practice

---
+++ SameLine.py
@@ -0,0 +1,13 @@
+# end and sep
+
+print("Hello,")
+print("World")
+
+print("__________________________________")
+
+print("Hello, ", end='')
+print("World!")
+
+# sep=''
+print("cat", "dog", "parrot")
+print("cat", "dog", "parrot", sep=',')
