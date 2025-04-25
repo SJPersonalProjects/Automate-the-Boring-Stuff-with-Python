bc39928b67d8ddefbffee58841308b103da5e6bf
Sarim Jokhio <sarimjokhioprivate@gmail.com>
2025-04-25 11:56:49 +0500

    practice random.shuffle and random.choice functions

---
+++ RandomChoice.py
@@ -0,0 +1,12 @@
+# Selects the random choice from the list using random class.
+
+import random
+
+pets = ["Dog", "Cat", "Moose"]
+print(random.choice(pets))
+
+print(random.choice(pets))
+
+print(random.choice(pets))
+
+print(random.choice(pets))
---
+++ RandomShuffle.py
@@ -0,0 +1,13 @@
+# Shuffles the list in random order using random.shuffle()
+
+import random
+
+people = ["Alice", "Bob", "Carol", "David"]
+
+random.shuffle(people)
+
+print(people)
+
+random.shuffle(people)
+
+print(people)
