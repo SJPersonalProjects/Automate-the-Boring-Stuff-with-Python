26cd02961a8348f81fff3789bc5fb73b551fe4ca
Sarim Jokhio <sarimjokhioprivate@gmail.com>
2025-04-26 17:14:11 +0500

    practice id() function

---
+++ IdFunction.py
@@ -0,0 +1,16 @@
+# Using id() function to obtain the value id.
+
+name = "Jake"
+
+print(id(name))
+
+value = 12
+
+print(id(value))
+
+values = [1, 2, 3, 4, 5]
+print(id(values))
+
+values.append(6)
+
+print(id(values))
