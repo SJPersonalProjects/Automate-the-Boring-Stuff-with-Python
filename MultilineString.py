0106391d5579936d3ef846688e0b52bb7df28eac
Sarim Jokhio <sarimjokhioprivate@gmail.com>
2025-05-02 17:16:24 +0500

    Tripple quote comments

--- MultilineString.py
+++ MultilineString.py
@@ -1,5 +1,7 @@
 # Practice usage of multiline strings.
 
+""""And this is a multiline comment as well."""
+
 print('''Dear Alice,
       Eve's cat has been arrested for catnapping, cat burglary, and extortion.
       
